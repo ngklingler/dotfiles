f7de81220f4d6949b8aa561944bfc8726680d048
Nick Klingler <ngklingler@gmail.com>
2019-12-06 10:26:17 -0500

    n alias for nnn that cd's on quit

--- shell_config.sh
+++ shell_config.sh
@@ -43,3 +43,29 @@ cd () {
     builtin cd "$@" && ls;
 }
 alias notes='vi ~/notes'
+
+n ()
+{
+    # Block nesting of nnn in subshells
+    if [ "${NNNLVL:-0}" -ge 1 ]; then
+        echo "nnn is already running"
+        return
+    fi
+
+    # The default behaviour is to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # To cd on quit only on ^G, export NNN_TMPFILE after the call to nnn
+    # NOTE: NNN_TMPFILE is fixed, should not be modified
+    export NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+
+    nnn "$@"
+
+    if [ -f "$NNN_TMPFILE" ]; then
+            . "$NNN_TMPFILE"
+            rm -f "$NNN_TMPFILE" > /dev/null
+    fi
+}
