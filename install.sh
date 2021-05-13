c4b32f435a88c581dcc5b3eebbcf70ef7fdac4d1
Nick Klingler <ngklingler@gmail.com>
2021-05-13 13:19:28 -0400

    add ipython startup file to use fzf for history searching, add to install.sh and make install.sh log more info

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ function chkmk () {
 }
 
 function dload () {
+    echo "downloading $1 to $2"
     chkmk $2
     if [ "$(command -v curl)" ]; then
         curl $1 -o $2
@@ -18,13 +19,16 @@ EOF
     fi
 }
 
+
 function symlink () {
     dir=$HOME/dotfiles
     backup=$HOME/dotfiles/old_dotfiles
+    # TODO this list is duplicated in the install function
     case "$1" in
         "shell_config.sh") files="bashrc bash_profile zshrc";;
         "vimrc") files="config/nvim/init.vim vimrc";;
         "config/alacritty/alacritty.yml") files="$1";;
+        "ipython/profile_default/startup/fzf.py") files="$1";;
     esac
 
     for file in $files; do
@@ -42,11 +46,11 @@ function install () {
     [ -d $HOME/dotfiles/old_dotfiles ] && rm -rf $HOME/dotfiles/old_dotfiles
     # TODO install git, pip, fzf
     [ -z "$(command -v nvr)" ] && python3 -m pip install --user neovim-remote
-    files="shell_config.sh vimrc config/alacritty/alacritty.yml"
+    files="shell_config.sh vimrc config/alacritty/alacritty.yml ipython/profile_default/startup/fzf.py"
     for f in $files; do
         symlink $f
     done
-    dload 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' "$HOME/.vim/autoload/plug.vim"
+    [ -f "$HOME./vim/autoload/plug.vim" ] || dload 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' "$HOME/.vim/autoload/plug.vim"
     [ -f $HOME/dotfiles/machine.sh ] || touch $HOME/dotfiles/machine.sh
     source $HOME/dotfiles/shell_config.sh
 }
