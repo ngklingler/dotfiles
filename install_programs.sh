b3a21f7d761c13279118c1cc60ea78c691de6948
Nick Klingler <me@nickk.org>
2022-06-17 10:12:25 -0400

    remove casks that only work on mac, install fzf via git clone rather than brew, remove code extensions that are installed by vs code settings sync

--- install_programs.sh
+++ install_programs.sh
@@ -7,11 +7,7 @@ source shell_config.sh
 [ -z "$(command -v rg)" ] && brew install ripgrep
 [ -z "$(command -v pipx)" ] && brew install pipx
 [ -z "$(command -v jq)" ] && brew install jq
-[ -z "$(command -v bw)" ] && brew install bitwarden-cli
-[ -z "$(command -v fzf)" ] && brew install fzf
-# casks don't work on linux
-[ -z "$(command -v firefox)" ] && brew install --cask firefox
-[ -z "$(command -v spotify)" ] && brew install --cask spotify
+
 
 # brew outputs bash completion file locations, capture and source
 # ==> Caveats
@@ -19,27 +15,8 @@ source shell_config.sh
 #   /home/linuxbrew/.linuxbrew/etc/bash_completion.d
 # https://docs.brew.sh/Shell-Completion
 
-code --install-extension 'bastienboutonnet.vscode-dbt'
-code --install-extension 'eamodio.gitlens'
-code --install-extension 'EditorConfig.EditorConfig'
-code --install-extension 'hashicorp.terraform'
-code --install-extension 'innoverio.vscode-dbt-power-user'
-code --install-extension 'kj.sqltools-driver-redshift'
-code --install-extension 'ms-azuretools.vscode-docker'
-code --install-extension 'ms-python.python'
-code --install-extension 'ms-python.vscode-pylance'
-code --install-extension 'ms-toolsai.jupyter'
-code --install-extension 'ms-toolsai.jupyter-keymap'
-code --install-extension 'ms-toolsai.jupyter-renderers'
-code --install-extension 'ms-vscode-remote.remote-containers'
-code --install-extension 'mtxr.sqltools'
-code --install-extension 'mtxr.sqltools-driver-pg'
-code --install-extension 'oderwat.indent-rainbow'
-code --install-extension 'richie5um2.vscode-sort-json'
-code --install-extension 'samuelcolvin.jinjahtml'
-code --install-extension 'tomoki1207.pdf'
-code --install-extension 'yzhang.markdown-all-in-one'
-code --install-extension 'zhuangtongfa.material-theme'
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 git clone https://github.com/Aloxaf/fzf-tab $HOME/dotfiles/fzf-tab/
 
