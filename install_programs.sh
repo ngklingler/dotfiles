94c7ac6c6b53d815cb342bd8328fd32c1f79fef3
Nicholas Klingler <16926414+ngklingler@users.noreply.github.com>
2026-02-13 10:38:30 -0500

    Refactor install_programs.sh to streamline installations
    Removed installation functions for asdf plugins and added commands for fzf and neovim installation.

--- install_programs.sh
+++ install_programs.sh
@@ -1,53 +1,14 @@
 source shell_config.sh
 
-# need curl and git
-asdf () {
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-    cd ~/.asdf
-    git checkout `git describe --tags --abbrev=0`
-    . ./asdf.sh
-    cd -
-}
-
-direnv () {
-    asdf direnv setup --shell bash --version latest
-    asdf global direnv latest
-}
-
-fd () {
-    asdf plugin add fd
-    asdf install fd latest
-    asdf global fd latest
-}
-
-jq () {
-    asdf plugin add jq
-    asdf install jq latest
-    asdf global jq latest
-}
-
-asdf
-direnv
-fd
-jq
-
-# TODO curl may not exist, also depends on git
-[ -z "$(command -v brew)" ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-[ -z "$(command -v rg)" ] && brew install ripgrep
-[ -z "$(command -v pipx)" ] && brew install pipx
-
-
-# brew outputs bash completion file locations, capture and source
-# ==> Caveats
-# Bash completion has been installed to:
-#   /home/linuxbrew/.linuxbrew/etc/bash_completion.d
-# https://docs.brew.sh/Shell-Completion
-
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 git clone https://github.com/Aloxaf/fzf-tab $HOME/dotfiles/fzf-tab/
 
 [ ! -z "$(command -v wget)" ] && wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -q
+
+[ ! -z "$(command -v wget)" ] && wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz -q && sudo tar xf nvim-linux-x86_64.tar.gz -C /usr/local/ && sudo ln -sf /usr/local/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim && rm -rf nvim-linux-x86_64.tar.gz
+
+# other things to install: rg, fd/find, pip, pip install neovim-remote
 
 source shell_config.sh
