d905cb80706d43f17d718df6d83cfbd48f3fb43a
Nicholas Klingler <16926414+ngklingler@users.noreply.github.com>
2026-02-13 10:44:16 -0500

    Remove fzf-tab installation from script
    Removed cloning of fzf-tab repository from dotfiles.

--- install_programs.sh
+++ install_programs.sh
@@ -3,8 +3,6 @@ source shell_config.sh
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-git clone https://github.com/Aloxaf/fzf-tab $HOME/dotfiles/fzf-tab/
-
 [ ! -z "$(command -v wget)" ] && wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -q
 
 [ ! -z "$(command -v wget)" ] && wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz -q && sudo tar xf nvim-linux-x86_64.tar.gz -C /usr/local/ && sudo ln -sf /usr/local/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim && rm -rf nvim-linux-x86_64.tar.gz
