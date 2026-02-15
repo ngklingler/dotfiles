560e4fd2af2d610d66dec510c995099c3ad44973
Nick Klingler <nick@localhost.localdomain>
2026-02-15 13:05:23 -0500

    fix visual mode highlighting

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -1,33 +1,34 @@
 return {
-	{
-		"navarasu/onedark.nvim",
-		version = "v0.1.0", -- Pin to legacy version
-		priority = 1000,
-		config = function()
-			require("onedark").setup({
-				style = "darker",
-				colors = {
-					bg0 = "#000000", -- main background
-					bg1 = "#000000",
-					bg2 = "#000000",
-					bg3 = "#000000",
-				},
+  {
+    "navarasu/onedark.nvim",
+    version = "v0.1.0", -- Pin to legacy version
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+        colors = {
+          bg0 = "#000000", -- main background
+          bg1 = "#000000",
+          bg2 = "#000000",
+          bg3 = "#000000",
+        },
 
-				highlights = {
-					Normal = { bg = "#000000" },
-					NormalFloat = { bg = "#000000" },
-					SignColumn = { bg = "#000000" },
-					EndOfBuffer = { bg = "#000000" },
-					CursorLine = { bg = "#111111" }, -- subtle cursor line contrast
-				},
-			})
-			require("onedark").load()
-		end,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "onedark",
-		},
-	},
+        highlights = {
+          Normal = { bg = "#000000" },
+          NormalFloat = { bg = "#000000" },
+          SignColumn = { bg = "#000000" },
+          EndOfBuffer = { bg = "#000000" },
+          CursorLine = { bg = "#111111" }, -- subtle cursor line contrast
+          Visual = { bg = "#222222" },
+        },
+      })
+      require("onedark").load()
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
 }
